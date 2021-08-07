23c75f283281bf4caac3f34e373223b3225289a4
GANESH CHANDRA <77651439+M-G-C-64@users.noreply.github.com>
2021-08-07 20:03:39 +0530

    Create RGB.py

---
+++ RGB.py
@@ -0,0 +1,32 @@
+'''No of ways of arranging the balls such that same ball should come side by side'''
+def sol(lst,ind,cnt):
+    if ind is None:
+        return 1
+    if ind == 0:
+        tlst = [lst[1],lst[2]]
+    elif ind == 1:
+        tlst = [lst[0],lst[2]]
+    else:
+        tlst = [lst[0],lst[1]]
+    
+    if (sum(x>0 for x in lst)) <= 1:
+        return 1
+    ind = next((i for i,v in enumerate(lst) if i and i != ind), None)
+    lst[ind] -= 1
+    #print('sfafd', sum(x>0 for x in tlst), tlst)
+    cnt = sum(x>0 for x in tlst)*sol(lst,ind,cnt)
+    
+    return cnt
+
+r = 2
+g = 2
+b = 2
+lst = [r,g,b]
+cnt = 0
+cnt += sum(x>0 for x in lst)
+ind = next((i for i,x in enumerate(lst) if x),None)
+if cnt > 0:
+    lst[ind] -= 1
+    print(cnt*sol(lst,ind,cnt))
+else:
+    print(cnt)
